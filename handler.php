79ede6f72e37230e16a6d08d118ed610f4b45870
jgadsbypeet <jgadsbypeet@gmail.com>
2015-11-02 16:02:36 +0000

    Removed more debug words

--- handler.php
+++ handler.php
@@ -9,7 +9,7 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        echo "";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -18,7 +18,7 @@ if(isset($_POST["submit"])) {
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "";
     $uploadOk = 0;
 }
 // Check file size
@@ -38,7 +38,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
